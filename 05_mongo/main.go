52f7830abf21134bc7846421633976f3837be8bc
wolingzong <276322154@qq.com>
2018-07-25 14:32:26 +0800

    mongo数据追加
    go操作mongo的demo

---
+++ 05_mongo/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type Person struct {
+	Name  string
+	Phone string
+}
+
+func main() {
+	// git clone http://gopkg.in/mgo.v2
+	session, err := mgo.Dial("mongodb://root:3Dclobest@3dcloserver:27017/Custom3DClo?authSource=admin&authMechanism=SCRAM-SHA-1")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	// Optional. Switch the session to a monotonic behavior.
+	session.SetMode(mgo.Monotonic, true)
+
+	c := session.DB("test").C("people")
+	err = c.Insert(&Person{"superWang", "13478808311"},
+		&Person{"David", "15040268074"})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result := Person{}
+	err = c.Find(bson.M{"name": "superWang"}).One(&result)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Name:", result.Name)
+	fmt.Println("Phone:", result.Phone)
+}
